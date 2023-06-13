9090432624a872d6b858d0117093e4358d120445
Valentin <Valentin@LAPTOP-UEKFOQCL>
2023-06-13 16:08:14 +0200

    find all personnel

--- SAE_01/SAE_01/Model/Personnel.cs
+++ SAE_01/SAE_01/Model/Personnel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,41 @@ namespace SAE_01.Model
 {
     class Personnel
     {
+        public Personnel()
+        {
+        }
 
-        public int idpersonnel { get; set; }
+        public Personnel(int idpersonnel, string emailpersonnel, string nompersonnel, string prenompersonnel)
+        {
+            this.Idpersonnel = idpersonnel;
+            this.Emailpersonnel = emailpersonnel;
+            this.Nompersonnel = nompersonnel;
+            this.Prenompersonnel = prenompersonnel;
+        }
+
+        public int Idpersonnel { get; set; }
+        public string Emailpersonnel { get; set; }
+        public string Nompersonnel { get; set; }
+        public string Prenompersonnel { get; set; }
+
+        public ObservableCollection<Personnel> FindAll()
+        {
+            ObservableCollection<Personnel> lesPersonnel = new ObservableCollection<Personnel>();
+            DataAccess accesBD = new DataAccess();
+            String requete = "select idpersonnel,emailpersonnel, nompersonnel, prenompersonnel from personnel ;";
+            DataTable datas = accesBD.GetData(requete);
+            if (datas != null)
+            {
+                foreach (DataRow row in datas.Rows)
+                {
+                    Personnel p = new Personnel(int.Parse(row["idpersonnel"].ToString()), (String)row["emailpersonnel"], (String)row["nompersonnel"], (String)row["prenompersonnel"]);
+                    lesPersonnel.Add(p);
+                }
+            }
+            return lesPersonnel;
+        }
+
+
 
     }
 }
