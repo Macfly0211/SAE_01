7c8ba503d17887eefedc93a901fa1700e53a868c
Colin Thomas <colin.thomas@etu.univ-smb.fr>
2023-06-13 16:42:26 +0200

    encore majuscule

---
+++ SAE_01/SAE_01/Model/EstAttribue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_01.Model
+{
+    public class EstAttribue
+    {
+        public EstAttribue(int idpersonnel, int idmateriel, DateTime date, string commentaireattribution)
+        {
+            this.Idpersonnel = idpersonnel;
+            this.Idmateriel = idmateriel;
+            this.Date = date;
+            this.Commentaireattribution = commentaireattribution;
+        }
+
+        public EstAttribue()
+        {
+            
+        }
+
+        public int Idpersonnel { get; set; }
+        public int Idmateriel { get; set; }
+        public DateTime Date { get; set; }
+        public string Commentaireattribution { get; set; }
+
+        public ObservableCollection<EstAttribue> FindAll()
+        {
+            ObservableCollection<EstAttribue> lesAttribution = new ObservableCollection<EstAttribue>();
+            DataAccess accesBD = new DataAccess();
+            String requete = "select idcategorie, nomcategorie from personnel ;";
+            DataTable datas = accesBD.GetData(requete);
+            if (datas != null)
+            {
+                foreach (DataRow row in datas.Rows)
+                {
+                    EstAttribue a = new EstAttribue(int.Parse(row["idpersonnel"].ToString()), int.Parse(row["idmateriel"].ToString()), (DateTime)row["date"], (string)row["commentaireattribution"]);
+                    lesAttribution.Add(a);
+                }
+            }
+            return lesAttribution;
+        }
+    }
+}
